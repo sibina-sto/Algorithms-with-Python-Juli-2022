467e6cef09d527f43f0454775914122c1061fb5d
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-07-06 22:07:38 +0300

    Update 07. RecursiveFibonacci.py

--- 1. Recursion/Lab - Recursion/07. RecursiveFibonacci.py
+++ 1. Recursion/Lab - Recursion/07. RecursiveFibonacci.py
@@ -1,8 +1,19 @@
-# def recur_fibo(n):
-#     if n <= 1:
-#         return n
-#     else:
-#         return(recur_fibo(n-1) + recur_fibo(n-2))
-#
-# n = int(input())
-# print(recur_fibo(n))
+def GetFibonacci(number):
+    if number<=1:
+        return 1
+    return GetFibonacci(number-1)+GetFibonacci(number-2)
+
+def iterative_fib(number):
+    fib0=1
+    fib1=1
+    result=0
+    for _ in range (number-1):
+        result=fib0+fib1
+        fib0,fib1=fib1,result
+    return result
+
+n=int(input())
+
+# print(GetFibonacci(n))
+
+print(iterative_fib(n))
