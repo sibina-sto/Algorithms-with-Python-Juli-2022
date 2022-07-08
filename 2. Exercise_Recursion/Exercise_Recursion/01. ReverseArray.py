eb74f539a681e01dfedb9eab5417c408e020a79c
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-07-08 08:49:51 +0300

    Update and rename 2. Exercise Recursion/Exercise_Recursion/01. ReverseArray.py to 2. Exercise_Recursion/Exercise_Recursion/01. ReverseArray.py

--- 2. Exercise_Recursion/Exercise_Recursion/01. ReverseArray.py
+++ 2. Exercise_Recursion/Exercise_Recursion/01. ReverseArray.py
@@ -1,5 +1,5 @@
 # def recursion_reverse_array(numbers):
-#    if len(numbers)==0:
+#    if len(numbers) == 0:
 #        return ''
 #    return f"{numbers.pop()} {recursion_reverse_array(numbers)}"
 
@@ -9,12 +9,12 @@
 
 Variant 2
 
-def reverse_array(idx,elements):
-    if idx==len(elements)//2:
+def reverse_array(idx, elements):
+    if idx == len(elements)//2:
         return
-    swap_idx=len(elements)-1-idx
-    elements[idx],elements[swap_idx]=elements[swap_idx],elements[idx]
-    reverse_array(idx+1, elements)
+    swap_idx = len(elements) - 1 - idx
+    elements[idx], elements[swap_idx] = elements[swap_idx], elements[idx]
+    reverse_array(idx + 1, elements)
 
 numbers = input().split()
 reverse_array(0, numbers)
