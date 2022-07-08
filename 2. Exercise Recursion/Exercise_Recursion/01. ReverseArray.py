5925111de800dd2521ade454e89101912f7ec381
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-07-08 09:30:44 +0300

    Update 01. ReverseArray.py

--- 2. Exercise Recursion/Exercise_Recursion/01. ReverseArray.py
+++ 2. Exercise Recursion/Exercise_Recursion/01. ReverseArray.py
@@ -1,8 +1,23 @@
-def recursion_reverse_array(numbers):
-    if len(numbers)==0:
-        return ''
-    return f"{numbers.pop()} {recursion_reverse_array(numbers)}"
 
+# Variant 1
+
+# def recursion_reverse_array(numbers):
+#     if len(numbers) == 0:
+#         return ''
+#     return f"{numbers.pop()} {recursion_reverse_array(numbers)}"
+#
+# numbers = input().split()
+# print(recursion_reverse_array(numbers))
+
+# Variant 2
+
+def reverse_array(idx,elements):
+    if idx == len(elements)//2:
+        return
+    swap_idx = len(elements) - 1 - idx
+    elements[idx], elements[swap_idx] = elements[swap_idx], elements[idx]
+    reverse_array(idx + 1, elements)
 
 numbers = input().split()
-print(recursion_reverse_array(numbers))
+reverse_array(0, numbers)
+print(' '.join(numbers))
