c664254c5ba3f600765c80ef2e180bddde677a78
Sibina Stoyanova <stoyanovasibina@gmail.com>
2022-07-19 10:03:05 +0300

    Update 05. Road_Reconstruction.py

--- 5. Exercise - Introduction to Graphs/Exercise/05. Road_Reconstruction.py
+++ 5. Exercise - Introduction to Graphs/Exercise/05. Road_Reconstruction.py
@@ -39,45 +39,46 @@ for first, second in edges:
     graph[second].append(first)
 
 # Variant 2
-from collections import defaultdict
 
-def dfs(node, graph, visited, component):
-    if visited[node] == True:
-        return
-    visited[node] = True
-    for child in graph[node]:
-        dfs(child, graph, visited, component)
-    component.append(node)
+# from collections import defaultdict
 
-cities = int(input())
-streets = int(input())
+# def dfs(node, graph, visited, component):
+#     if visited[node] == True:
+#         return
+#     visited[node] = True
+#     for child in graph[node]:
+#         dfs(child, graph, visited, component)
+#     component.append(node)
 
-graph = defaultdict(list)
-edges = []
+# cities = int(input())
+# streets = int(input())
 
-for _ in range(streets):
-    edge_parts = input().split(' - ')
-    edges.append(edge_parts)
-    graph[edge_parts[0]].append(edge_parts[1])
-    graph[edge_parts[1]].append(edge_parts[0])
+# graph = defaultdict(list)
+# edges = []
 
-important_streets = []
+# for _ in range(streets):
+#     edge_parts = input().split(' - ')
+#     edges.append(edge_parts)
+#     graph[edge_parts[0]].append(edge_parts[1])
+#     graph[edge_parts[1]].append(edge_parts[0])
 
-for edge in sorted(edges):
-    start_node = edge[0]
-    end_node = edge[1]
-    graph[start_node].remove(end_node)
-    graph[end_node].remove(start_node)
-    component = []
-    visited = {}
-    for node in graph.keys():
-        visited[node] = False
-    dfs(start_node, graph, visited, component)
-    if end_node not in component:
-        sorted_edge = sorted([start_node, end_node])
-        important_streets.append(f"{sorted_edge[0]} {sorted_edge[1]}")
-    graph[start_node].append(end_node)
-    graph[end_node].append(start_node)
+# important_streets = []
 
-print("Important streets:")
-[print(street) for street in important_streets]
+# for edge in sorted(edges):
+#     start_node = edge[0]
+#     end_node = edge[1]
+#     graph[start_node].remove(end_node)
+#     graph[end_node].remove(start_node)
+#     component = []
+#     visited = {}
+#     for node in graph.keys():
+#         visited[node] = False
+#     dfs(start_node, graph, visited, component)
+#     if end_node not in component:
+#         sorted_edge = sorted([start_node, end_node])
+#         important_streets.append(f"{sorted_edge[0]} {sorted_edge[1]}")
+#     graph[start_node].append(end_node)
+#     graph[end_node].append(start_node)
+
+# print("Important streets:")
+# [print(street) for street in important_streets]
